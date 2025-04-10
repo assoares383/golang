665ccd9c2d684bb2aaf88ba2152d1a28f55bda79
= <alexandressoares383@gmail.com>
2025-04-10 19:26:26 -0300

    feat: json get request

--- 03.integrando-uma-lib-framework/integrando-com-chi-e-middlewares/main.go
+++ 03.integrando-uma-lib-framework/integrando-com-chi-e-middlewares/main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
-	"time"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+
+type User struct {
+	Username string
+	ID       int64 `json: ", string"`
+	Role     string
+	Password string `json:"-"`
+}
 
 func main() {
 	r := chi.NewMux()
 
-	r.Get("/horario", func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now()
-		fmt.Fprintln(w, now)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	db := map[int64]User{
+		1: {
+			ID:       1,
+			Username: "admin",
+			Password: "admin",
+			Role:     "admin",
+		},
+	}
+
+	r.Group(func(r chi.Router) {
+		r.Use(jsonMiddleware)
+		r.Get("/users/{id:[0-9]+}", handleGetUsers(db))
+		r.Post("/users", handlePostUsers)
 	})
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
 	}
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handleGetUsers(db map[int64]User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		idStr := chi.URLParam(r, "id")
+		id, _ := strconv.ParseInt(idStr, 10, 64)
+
+		user, ok := db[id]
+
+		if ok {
+			data, err := json.Marshal(user)
+
+			if err != nil {
+				panic(err)
+			}
+
+			w.Write(data)
+		}
+	}
+}
+
+func handlePostUsers(w http.ResponseWriter, r *http.Request) {
+
 }
